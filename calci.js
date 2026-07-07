7c1ce0a5843eb80b37aee7614b93d63293c31cbc
thotavishwath-com <thotavishwath@gmail.com>
2026-07-07 20:03:15 +0530

    calci.js

---
+++ calci.js
@@ -0,0 +1,20 @@
+function add(x, y) {
+    return x + y;
+}
+
+function sub(x, y) {
+    return x - y;
+}
+
+function mult(x, y) {
+    return x * y;
+}
+
+function div(x, y) {
+    return x / y;
+}
+
+console.log("Addition:", add(10, 5));
+console.log("Subtraction:", sub(10, 5));
+console.log("Multiplication:", mult(10, 5));
+console.log("Division:", div(10, 5));
